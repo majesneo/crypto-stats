b3535800cb2e1bbc5c44b1231bf9fa142ac8d5b3
lev <lev.typets@softomate.com>
2022-10-24 14:59:39 +0600

    add feature-sliced, axios, fetcher

---
+++ src/shared/api/models/axios.ts
@@ -0,0 +1,42 @@
+import axios, {AxiosInstance} from "axios";
+import {FetcherI} from "../api";
+
+
+const headers: Readonly<Record<string, string | boolean>> = {
+  Accept: "application/json",
+  "Content-Type": "application/json; charset=utf-8",
+  "Access-Control-Allow-Credentials": true,
+  "X-Requested-With": "XMLHttpRequest",
+};
+
+
+
+export class Axios implements FetcherI {
+  private static instance: Axios
+
+  private get axiosInstance(): AxiosInstance {
+    return this.axiosInitInstance(this.baseURL)
+  }
+
+  constructor(public baseURL: string) {
+    if (Axios.instance) {
+      return Axios.instance
+    } else {
+      Axios.instance = this
+      this.axiosInitInstance(baseURL)
+    }
+  }
+
+  axiosInitInstance(baseURL: string) {
+    return axios.create({
+      baseURL,
+      headers,
+      withCredentials: false,
+    })
+  }
+
+  get(url: string) {
+    return this.axiosInstance.get(url)
+  }
+
+}
