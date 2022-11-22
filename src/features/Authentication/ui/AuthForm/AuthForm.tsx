efd1aca0d4cd390942a7f516f25fb366a29d70db
lev <lev.typets@softomate.com>
2022-11-22 10:06:11 +0600

    control component

--- src/features/Authentication/ui/AuthForm/AuthForm.tsx
+++ src/features/Authentication/ui/AuthForm/AuthForm.tsx
@@ -1,40 +1,43 @@
-import React, { FC, ReactElement, useRef } from 'react';
+import React, { FC, ReactElement, useRef, useState } from 'react';
 import { STATUS } from '../../../../shared/constants/constants';
 import { Field } from '../../../../shared/ui/components/Field/FieldGroup';
 import { StyledForm } from './style';
 
 interface AuthFormProps {
   login: () => {
-    email?: string;
-    password?: string;
+    email: string;
+    password: string;
   };
 }
 
 export interface AuthFormI {
   children: (props: AuthFormProps) => ReactElement;
-  status: keyof typeof STATUS;
 }
 
-export const AuthForm: FC<AuthFormI> = ({ children, status }) => {
-  const inputPassword = useRef<HTMLInputElement>();
-  const inputEmail = useRef<HTMLInputElement | null>(null);
+export const AuthForm: FC<AuthFormI> = ({ children }) => {
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
 
   function onSubmit() {
     return {
-      password: inputPassword?.current?.value,
-      email: inputEmail?.current?.value,
+      password,
+      email
     };
   }
 
   return (
-    <StyledForm>
+    <StyledForm onSubmit={onSubmit}>
       <Field>
         <Field.Label>Email</Field.Label>
-        <Field.Input ref={inputEmail} />
+        <Field.Input 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)} />
       </Field>
       <Field>
         <Field.Label>Password</Field.Label>
-        <Field.Input ref={inputPassword} type="password" />
+        <Field.Input value={password} 
+        onChange={(e)=>setPassword(e.target.value)} 
+        type="password" />
       </Field>
       {children({
         login: () => onSubmit(),
